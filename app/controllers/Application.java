582c15cfbfde05fad5d46f30d7812b0fa99651ef
iTAP <iTAP@iTAP-PC01>
2015-05-25 13:29:35 +0900

    delete postManage&userManage, add all screen's process(return TODO)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,77 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+
     public static Result login(){
     	return TODO;
     }
-}
+
+    public static Result adminMenu(){
+    	return TODO;
+    }
+
+    public static Result thanksEntry(){
+    	return TODO;
+    }
+
+    public static Result boardList(){
+    	return TODO;
+    }
+
+    public static Result board(){
+    	return TODO;
+    }
+
+    public static Result send(){
+    	return TODO;
+    }
+
+    public static Result receive(){
+    	return TODO;
+    }
+
+    public static Result userManage(){
+    	return TODO;
+    }
+
+    public static Result postManage(){
+    	return TODO;
+    }
+
+    public static Result addUser(){
+    	return TODO;
+    }
+
+    public static Result addClass(){
+    	return TODO;
+    }
+
+    public static Result check(){
+    	return TODO;
+    }
+
+    public static Result deleteUser(){
+    	return TODO;
+    }
+
+    public static Result deletePost(){
+    	return TODO;
+    }
+
+    public static Result deleteClass(){
+    	return TODO;
+    }
+
+    public static Result vote(){
+    	return TODO;
+    }
+
+    public static Result search(){
+    	return TODO;
+    }
+
+    public static Result count(){
+    	return TODO;
+    }
+
+   }
