c7549be65d78fc87dd03bbbd57b4236e011dc38f
User <User@H27-NotePC09>
2015-05-25 13:39:46 +0900

    権限テーブルの追加

--- app/models/Login.java
+++ app/models/Login.java
@@ -8,11 +8,7 @@ import play.db.ebean.Model;
 @Entity
 public class Login extends Model {
 	@Id
-    public Integer employee_number;
+    public Integer id;
 
-	public Integer user_number;
-
-    public String password;
-
-    public Integer permission_id;
+    public String name;
 }
