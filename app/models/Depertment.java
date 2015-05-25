dec20eb2e6f48d8afb3e1de87a472596898c1b24
User <User@H27-NotePC09>
2015-05-25 14:33:18 +0900

    部署テーブルの追加

---
+++ app/models/Depertment.java
@@ -0,0 +1,14 @@
+
+package models;
+
+import play.db.ebean.Model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+public class Depertment extends Model {
+	@Id//←ここでIDを主キーに設定している
+    public Integer department_id;
+
+    public String department_name;
+
+}
